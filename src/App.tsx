f01b0894f3204f0919b144a8d0cb418fee4188ce
Kavtorev <dmitrijkavtorev@gmail.com>
2021-08-31 12:31:20 +0200

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import { GlobalStyle } from './styles/GlobalStyle';
+import { CartPage, ProductDescriptionPage, ProductListingPage } from './pages';
+import { Normalize } from 'styled-normalize';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=''>
+      <GlobalStyle />
+      <Normalize />
+      <h1>Header!</h1>
+      <Switch>
+        <Route path='/' exact component={ProductListingPage} />
+        <Route path='/cart' component={CartPage} />
+        <Route path='/details/:id' component={ProductDescriptionPage} />
+      </Switch>
     </div>
   );
 }
