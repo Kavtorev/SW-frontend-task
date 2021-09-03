9fc17512d873fed78eae19c473a18e098c64d610
Kavtorev <dmitrijkavtorev@gmail.com>
2021-09-03 23:27:25 +0200

    Header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import {
+  StyledHeader,
+  List,
+  ActionsWrapper,
+  Navigation,
+  LogoWrapper,
+  Item,
+} from './styles';
+import logosrc from './assets/Brand icon.svg';
+import Cart from '../Cart/Cart';
+import CurrencySwitcher from '../CurrencySwitcher/CurrencySwitcher';
+import { connector, PropsFromRedux } from '../../store';
+import { NavLink } from 'react-router-dom';
+
+interface Props extends PropsFromRedux {}
+
+class Header extends React.Component<Props> {
+  render() {
+    const { categories, selectCategory } = this.props;
+    return (
+      <StyledHeader>
+        <Navigation>
+          <List>
+            {categories.map(({ name }) => (
+              <Item key={name}>
+                <NavLink
+                  onClick={() => selectCategory(name)}
+                  to={`/${name}`}
+                  activeStyle={{
+                    borderBottom: '2px solid var(--c-primary)',
+                    color: 'var(--c-primary)',
+                    fontWeight: 600,
+                  }}
+                >
+                  {name}
+                </NavLink>
+              </Item>
+            ))}
+          </List>
+        </Navigation>
+        <LogoWrapper>
+          <img src={logosrc} alt='Logo' />
+        </LogoWrapper>
+        <ActionsWrapper>
+          <CurrencySwitcher />
+          <Cart />
+        </ActionsWrapper>
+      </StyledHeader>
+    );
+  }
+}
+
+// border-bottom: 2px solid var(--c-primary);
+
+export default connector(Header);
---
+++ src/components/Header/styles.tsx
@@ -0,0 +1,59 @@
+import styled from 'styled-components';
+
+export const StyledHeader = styled.header`
+  position: sticky;
+  top: 0;
+  z-index: 999;
+  background-color: white;
+  max-width: var(--container-width);
+  height: 80px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0 auto;
+`;
+
+export const Navigation = styled.nav`
+  height: 100%;
+`;
+
+export const List = styled.ul`
+  height: inherit;
+  & > li {
+    display: inline-block;
+    text-transform: uppercase;
+    margin-right: var(--m-s);
+    padding-top: calc(var(--p-l) + 0.5em);
+    height: 100%;
+    text-align: center;
+    width: 97px;
+    line-height: 1.1875;
+    cursor: pointer;
+  }
+`;
+
+export const Item = styled.li`
+  & > a {
+    display: block;
+    height: 100%;
+    text-decoration: none;
+    color: var(--c-black);
+  }
+`;
+
+export const LogoWrapper = styled.span`
+  position: absolute;
+  top: 60%;
+  left: 50%;
+  transform: translate(-60%, -50%);
+`;
+
+export const ActionsWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  & > * {
+    margin-left: 1.375em;
+  }
+`;
