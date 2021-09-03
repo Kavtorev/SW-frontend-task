3bc81f04f74cf7890ac707edee9aff7023ad8835
Kavtorev <dmitrijkavtorev@gmail.com>
2021-09-03 23:25:07 +0200

    OutOfStockHolder

---
+++ src/components/OutOfStockHolder/index.tsx
@@ -0,0 +1,1 @@
+export { OutOfStockHolder } from './styles';
---
+++ src/components/OutOfStockHolder/styles.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+export const OutOfStockHolder = styled.p`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: var(--fs-m);
+  text-transform: uppercase;
+  color: var(--c-disabled);
+`;
