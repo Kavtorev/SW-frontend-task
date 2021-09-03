030bad79e57b8c717107bec0dbc327ae99864703
Kavtorev <dmitrijkavtorev@gmail.com>
2021-09-03 23:28:07 +0200

    shared

---
+++ src/shared/interfaces.tsx
@@ -0,0 +1,33 @@
+export interface IPrice {
+  currency: string;
+  amount: number;
+}
+
+export interface IProduct {
+  id: string;
+  name: string;
+  inStock: boolean;
+  gallery: string[];
+  description: string;
+  category: string;
+  prices: IPrice[];
+  brand: string;
+  attributes: IAttributeSet[];
+}
+
+export interface ICategory {
+  name: string;
+}
+
+export interface IAttributeSet {
+  id: string;
+  name: string;
+  type: string;
+  items: IAttribute[];
+}
+
+export interface IAttribute {
+  displayValue: string;
+  value: string;
+  id: string;
+}
---
+++ src/shared/mappers.tsx
@@ -0,0 +1,7 @@
+export const currencyMapper: { [currencyId: string]: string } = {
+  USD: '$',
+  JPY: '¥',
+  EUR: '€',
+  RUB: '₽',
+  GBP: '£',
+};
