3dd35ee830077e859a5a7eb404704a64e1b2c438
Kavtorev <dmitrijkavtorev@gmail.com>
2021-09-05 00:11:47 +0200

    updated common

--- src/common/Utils/styles.tsx
+++ src/common/Utils/styles.tsx
@@ -5,6 +5,16 @@ export const ScrollY = css`
   scrollbar-width: none;
 `;
 
+export const ScrollX = css`
+  overflow-x: scroll;
+  scrollbar-width: none;
+`;
+
+export const ScrollBoth = css`
+  overflow: scroll;
+  scrollbar-width: none;
+`;
+
 export const Truncate = css`
   overflow: hidden;
   text-overflow: ellipsis;
