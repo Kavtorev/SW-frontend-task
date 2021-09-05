045a289f374868af4011f2b1f6af52b594951257
Kavtorev <dmitrijkavtorev@gmail.com>
2021-09-06 00:59:45 +0200

    scrollbar fixes

--- src/common/Utils/styles.tsx
+++ src/common/Utils/styles.tsx
@@ -3,16 +3,30 @@ import { css } from 'styled-components';
 export const ScrollY = css`
   overflow-y: scroll;
   scrollbar-width: none;
+  overflow-x: hidden;
+  -ms-overflow-style: none;
+  ::-webkit-scrollbar {
+    display: none;
+  }
 `;
 
 export const ScrollX = css`
   overflow-x: scroll;
   scrollbar-width: none;
+  overflow-y: hidden;
+  -ms-overflow-style: none;
+  ::-webkit-scrollbar {
+    display: none;
+  }
 `;
 
 export const ScrollBoth = css`
   overflow: scroll;
   scrollbar-width: none;
+  -ms-overflow-style: none;
+  ::-webkit-scrollbar {
+    display: none;
+  }
 `;
 
 export const Truncate = css`
