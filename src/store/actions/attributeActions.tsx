82d0ab41c58a11e00d05dd101231d06777503127
Kavtorev <dmitrijkavtorev@gmail.com>
2021-09-09 23:29:03 +0200

    cart/attribute actions

--- src/store/actions/attributeActions.tsx
+++ src/store/actions/attributeActions.tsx
@@ -1,18 +1,27 @@
-import { IAttribute, IAttributeSet, IProduct } from '../../shared';
+import {
+  IAttribute,
+  IAttributeSet,
+  IProduct,
+  IAttributeMeta,
+} from '../../shared';
 
 export type AttributeActionsType =
+  | { type: 'REMOVE_ALL_ATTRIBUTE_SELECTIONS'; payload: string }
   | {
-      type: 'SELECT_ATTRIBUTE';
+      type: 'SELECT_LOCAL';
       payload: {
         productId: IProduct['id'];
         meta: { attrId: IAttributeSet['id']; itemId: IAttribute['id'] };
       };
     }
   | {
-      type: 'UNSELECT_ATTRIBUTE';
-      payload: {
-        productId: IProduct['id'];
-        attrId: IAttributeSet['id'];
-      };
+      type: 'UNSELECT_LOCAL';
+      payload: { productId: IProduct['id']; attrId: IAttributeSet['id'] };
     }
-  | { type: 'REMOVE_ALL_ATTRIBUTE_SELECTIONS'; payload: IProduct['id'] };
+  | {
+      type: 'SET_SELECTED_ATTRIBUTES';
+      payload: {
+        composedId: string;
+        selectedAttributes: Array<IAttributeMeta>;
+      };
+    };
--- src/store/actions/cartActions.tsx
+++ src/store/actions/cartActions.tsx
@@ -1,9 +1,12 @@
 import { IProduct } from '../../shared';
 
 export type CartActionsType =
-  | { type: 'ADD_PRODUCT_TO_CART'; payload: IProduct }
+  | {
+      type: 'ADD_PRODUCT_TO_CART';
+      payload: { composedId: string; product: IProduct; quantity: number };
+    }
   | { type: 'REMOVE_PRODUCT_FROM_CART'; payload: IProduct['id'] }
   | {
       type: 'CHANGE_PRODUCT_QUANTITY';
-      payload: { productId: IProduct['id']; quantity: number };
+      payload: { composedId: IProduct['id']; quantity: number };
     };
