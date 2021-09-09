171541ba725d8674c129b8664d2c1f792699049b
Kavtorev <dmitrijkavtorev@gmail.com>
2021-09-09 23:32:25 +0200

    redux updated mappers

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -3,21 +3,27 @@ import { connect, ConnectedProps } from 'react-redux';
 import {
   uiReducer,
   requestRemoteDataReducer,
-  cartRedcucer,
+  cartReducer,
   attributeReducer,
 } from './reducers';
 import { createLogger } from 'redux-logger';
 import { requestInitialData, requestProductsByCategoryName } from './actions';
-import { IProduct, IAttributeSet, IAttribute, ICategory } from '../shared';
+import {
+  IProduct,
+  IAttributeSet,
+  IAttribute,
+  ICategory,
+  IAttributeMeta,
+} from '../shared';
 
 export const rootReducers = combineReducers({
   uiReducer,
   requestRemoteDataReducer,
-  cartRedcucer,
+  cartRedcucer: cartReducer,
   attributeReducer,
 });
-// const logger = createLogger();
-export const store = createStore(rootReducers, applyMiddleware());
+const logger = createLogger();
+export const store = createStore(rootReducers, applyMiddleware(logger));
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
@@ -39,14 +45,16 @@ const mapState = (state: RootState) => ({
     totalQuantity: state.cartRedcucer.total,
   },
   fetchedProducts: state.requestRemoteDataReducer.products,
-  attributeSelections: state.attributeReducer.mappedAttributeSelections,
+  cartProductAttributeSelections:
+    state.attributeReducer.cartProductAttributeSelections,
+  localProductAttributeSelections:
+    state.attributeReducer.localProductAttributeSelections,
 });
 
 const mapDispatch = (dispatch: AppDispatch) => ({
   closeOrOpenCartPopper: () => dispatch({ type: 'CLOSE_OR_OPEN_CART_POPPER' }),
 
-  closeOrOpenCurrencyMenu: () =>
-    dispatch({ type: 'CLOSE_OR_OPEN_CURRENCY_MENU' }),
+  toggleCurrencyMenu: () => dispatch({ type: 'CLOSE_OR_OPEN_CURRENCY_MENU' }),
 
   selectCurrency: (payload: string) =>
     dispatch({ type: 'SELECT_CURRENCY', payload }),
@@ -59,39 +67,47 @@ const mapDispatch = (dispatch: AppDispatch) => ({
   fetchProductsByCategoryName: (categoryName: ICategory['name']) =>
     requestProductsByCategoryName(dispatch, categoryName),
 
-  addProductToCart: (product: IProduct) =>
-    dispatch({ type: 'ADD_PRODUCT_TO_CART', payload: product }),
-
-  removeProductFromCart: (productId: IProduct['id']) =>
-    dispatch({ type: 'REMOVE_PRODUCT_FROM_CART', payload: productId }),
-
-  changeProductQuantity: ({
-    productId,
-    quantity,
-  }: {
-    productId: IProduct['id'];
-    quantity: number;
-  }) =>
+  addProductToCart: (composedId: string, product: IProduct, quantity: number) =>
     dispatch({
-      type: 'CHANGE_PRODUCT_QUANTITY',
-      payload: { productId, quantity },
+      type: 'ADD_PRODUCT_TO_CART',
+      payload: { composedId, product, quantity },
     }),
 
-  selectAttribute: (
+  removeProductFromCart: (composedId: string) =>
+    dispatch({ type: 'REMOVE_PRODUCT_FROM_CART', payload: composedId }),
+
+  changeProductQuantity: (composedId: string, quantity: number) =>
+    dispatch({
+      type: 'CHANGE_PRODUCT_QUANTITY',
+      payload: { composedId, quantity },
+    }),
+
+  selectLocalAttribute: (
     productId: IProduct['id'],
     attrId: IAttributeSet['id'],
     itemId: IAttribute['id']
   ) =>
     dispatch({
-      type: 'SELECT_ATTRIBUTE',
+      type: 'SELECT_LOCAL',
       payload: { productId, meta: { attrId, itemId } },
     }),
 
-  unSelectAttribute: (productId: IProduct['id'], attrId: IAttributeSet['id']) =>
-    dispatch({ type: 'UNSELECT_ATTRIBUTE', payload: { productId, attrId } }),
+  unSelectLocalAttribute: (
+    productId: IProduct['id'],
+    attrId: IAttributeSet['id']
+  ) => dispatch({ type: 'UNSELECT_LOCAL', payload: { productId, attrId } }),
 
-  removeAllAttributeSelections: (productId: IProduct['id']) =>
-    dispatch({ type: 'REMOVE_ALL_ATTRIBUTE_SELECTIONS', payload: productId }),
+  setSelectedAttributes: (
+    composedId: string,
+    selectedAttributes: Array<IAttributeMeta>
+  ) =>
+    dispatch({
+      type: 'SET_SELECTED_ATTRIBUTES',
+      payload: { composedId, selectedAttributes },
+    }),
+
+  removeAllAttributeSelections: (composedId: IProduct['id']) =>
+    dispatch({ type: 'REMOVE_ALL_ATTRIBUTE_SELECTIONS', payload: composedId }),
 });
 
 export const connector = connect(mapState, mapDispatch);
