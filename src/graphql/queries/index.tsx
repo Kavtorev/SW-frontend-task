0235061295c453322a487ec1fa3711a22635a0fd
Kavtorev <dmitrijkavtorev@gmail.com>
2021-09-11 21:54:31 +0200

    core product fields fragment

--- src/graphql/queries/index.tsx
+++ src/graphql/queries/index.tsx
@@ -1,5 +1,31 @@
 import { gql } from '@apollo/client';
 
+const CORE_PRODUCT_FIELDS = gql`
+  fragment CoreProductFields on Product {
+    id
+    category
+    name
+    brand
+    inStock
+    description
+    gallery
+    prices {
+      currency
+      amount
+    }
+    attributes {
+      id
+      name
+      type
+      items {
+        id
+        displayValue
+        value
+      }
+    }
+  }
+`;
+
 export const GET_INITIAL_DATA = gql`
   query GetInitialData {
     categories {
@@ -10,60 +36,22 @@ export const GET_INITIAL_DATA = gql`
 `;
 
 export const GET_PRODUCTS_BY_CATEGORY_NAME = gql`
+  ${CORE_PRODUCT_FIELDS}
   query GetProductsByCategoryName($categoryName: String!) {
     category(input: { title: $categoryName }) {
       name
       products {
-        id
-        category
-        name
-        brand
-        inStock
-        description
-        gallery
-        prices {
-          currency
-          amount
-        }
-        attributes {
-          id
-          name
-          type
-          items {
-            id
-            displayValue
-            value
-          }
-        }
+        ...CoreProductFields
       }
     }
   }
 `;
 
 export const GET_PRODUCT_BY_ID = gql`
+  ${CORE_PRODUCT_FIELDS}
   query GetProductById($productId: String!) {
     product(id: $productId) {
-      id
-      category
-      name
-      brand
-      inStock
-      description
-      gallery
-      prices {
-        currency
-        amount
-      }
-      attributes {
-        id
-        name
-        type
-        items {
-          id
-          displayValue
-          value
-        }
-      }
+      ...CoreProductFields
     }
   }
 `;
