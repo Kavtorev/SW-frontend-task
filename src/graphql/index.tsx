e7d9c1198fc24cbda2805210ff3d7324883f9684
Kavtorev <dmitrijkavtorev@gmail.com>
2021-09-12 02:07:24 +0200

    disabled apollo cache to avoid its buggy nature

--- src/graphql/index.tsx
+++ src/graphql/index.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  DefaultOptions,
+} from '@apollo/client';
+
+const defaultOptions: DefaultOptions = {
+  watchQuery: {
+    fetchPolicy: 'no-cache',
+    errorPolicy: 'ignore',
+  },
+  query: {
+    fetchPolicy: 'no-cache',
+    errorPolicy: 'all',
+  },
+};
 
 export const client = new ApolloClient({
   uri: 'http://localhost:4000',
   cache: new InMemoryCache(),
+  defaultOptions,
 });
 
 // not neccessary tbh
